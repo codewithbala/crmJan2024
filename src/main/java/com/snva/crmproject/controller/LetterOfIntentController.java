cbdedae5193383eacec667344ed0a70b8978ab57
kkabaso08 <153551820+kkabaso08@users.noreply.github.com>
2024-01-18 11:34:06 -0600

    Added Controller

---
+++ src/main/java/com/snva/crmproject/controller/LetterOfIntentController.java
@@ -0,0 +1,59 @@
+package com.snva.crmproject.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.snva.crmproject.entity.LetterOfIntent;
+import com.snva.crmproject.service.LetterOfIntentService;
+
+@RestController
+@RequestMapping("/api/letter-of-intent")
+public class LetterOfIntentController {
+	private static final Logger LOGGER = LoggerFactory.getLogger(LetterOfIntentController.class);
+
+	@Autowired
+	private LetterOfIntentService letterOfIntentService;
+
+	@GetMapping
+	public List<LetterOfIntent> getAllLettersOfIntent() {
+		LOGGER.info("Getting all Letters of Intent");
+		return letterOfIntentService.getAllLettersOfIntent();
+	}
+
+	@GetMapping("/{id}")
+	public Optional<LetterOfIntent> getLetterOfIntentById(@PathVariable Long id) {
+		LOGGER.info("Getting Letter of Intent by ID: {}", id);
+		return letterOfIntentService.getLetterOfIntentById(id);
+	}
+
+	@PostMapping
+	public LetterOfIntent saveLetterOfIntent(@RequestBody LetterOfIntent letterOfIntent) {
+		LOGGER.info("Saving Letter of Intent");
+		return letterOfIntentService.saveLetterOfIntent(letterOfIntent);
+	}
+
+	/*
+	 * @DeleteMapping("/{id}") public void deleteLetterOfIntent(@PathVariable Long
+	 * id) { LOGGER.info("Deleting Letter of Intent by ID: {}", id);
+	 * letterOfIntentService.deleteLetterOfIntent(id); }
+	 */
+
+	@PutMapping("update-bd-candidate")
+	public void updateBDCandidate(@RequestBody LetterOfIntent letterOfIntent) {
+		LOGGER.info("Updating BD Candidate for Letter of Intent with ID: {}", letterOfIntent.getId());
+		letterOfIntentService.updateBDCandidate(letterOfIntent);
+	}
+
+}

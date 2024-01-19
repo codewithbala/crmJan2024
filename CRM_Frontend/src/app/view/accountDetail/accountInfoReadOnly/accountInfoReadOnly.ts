550e7d9988e55c8aff5d8339ed2a419805eb2442
lyc32 <liyc930826@gmail.com>
2024-01-18 18:26:08 -0600

    add UIconfigure

--- CRM_Frontend/src/app/view/accountDetail/accountInfoReadOnly/accountInfoReadOnly.ts
+++ CRM_Frontend/src/app/view/accountDetail/accountInfoReadOnly/accountInfoReadOnly.ts
@@ -1,5 +1,6 @@
 import {Component, Input} from '@angular/core';
 import {DeleteUserConfirmView} from "../../popView/deleteUserConfirmView/deleteUserConfirmView";
+import {UIconfigure} from "../../../config/UIconfigure";
 
 @Component({
   selector: 'app-account-info-read-only',
@@ -9,4 +10,9 @@ import {DeleteUserConfirmView} from "../../popView/deleteUserConfirmView/deleteU
 export class AccountInfoReadOnly
 {
   @Input() currentUser:any;
+  imageUrl: string | ArrayBuffer | null = "https://media.istockphoto.com/id/522855255/vector/male-profile-flat-blue-simple-icon-with-long-shadow.jpg?s=612x612&w=0&k=20&c=EQa9pV1fZEGfGCW_aEK5X_Gyob8YuRcOYCYZeuBzztM=";
+
+  constructor(public uiconfigure:UIconfigure)
+  {
+  }
 }

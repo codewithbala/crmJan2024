3489aa0e71821da721882c7c2dab00ef07f8574e
Ryhor Mudrahel <ryhor.murahel@gmail.com>
2024-01-22 15:13:02 -0500

    BatchCandidateControllerTest - test enhancement in progress

--- src/test/java/com/snva/crmproject/controller/BatchCandidateControllerTest.java
+++ src/test/java/com/snva/crmproject/controller/BatchCandidateControllerTest.java
@@ -1,15 +1,20 @@
 package com.snva.crmproject.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.jayway.jsonpath.JsonPath;
 import com.snva.crmproject.CrmProjectApplication;
+import com.snva.crmproject.entity.CandidateBasicDetails;
 import com.snva.crmproject.entity.batchDetails.Batch;
 import com.snva.crmproject.entity.batchDetails.BatchCandidates;
-import com.snva.crmproject.entity.batchDetails.BatchCandidatesId;
 import com.snva.crmproject.entity.CandidateDetails;
+import com.snva.crmproject.repository.batch.BatchRepository;
+import com.snva.crmproject.repository.candidate.CandidateBasicDetailsRepository;
+import com.snva.crmproject.repository.candidate.CandidateDetailsRepository;
 import com.snva.crmproject.utility.TestUser;
 import org.junit.jupiter.api.*;
+
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,7 +23,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -27,78 +31,133 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 public class BatchCandidateControllerTest {
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(BatchCandidateControllerTest.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(BatchCandidateControllerTest.class);
 
-	private static String authorization = TestUser.getBase64Credentials();
+    private static String authorization = TestUser.getBase64Credentials();
 
-	@Autowired
-	private MockMvc mockMvc;
+    @Autowired
+    private MockMvc mockMvc;
 
-	@Autowired
-	private ObjectMapper objectMapper;
+    @Autowired
+    private ObjectMapper objectMapper;
 
-	@Test
-	public void testCreateBatchCandidate() throws Exception {
-		LOGGER.info("Creating a batch candidate for testing");
+    private static Batch testingBatch;
+    private static CandidateDetails testingCandidate;
+    private static CandidateBasicDetails testingCandidateBD;
 
-		Date startDate = new Date();
-		Batch batch = new Batch(2L, startDate, "regular", "upcoming");
-		CandidateDetails candidate = new CandidateDetails("C124", null, 0, null, null, null, null, null, null, null,
-				null, null, null, null, null, false, false, false, null);
-		BatchCandidates batchCandidate = new BatchCandidates(batch, candidate);
+    @Autowired
+    private BatchRepository batchRepository;
+    @Autowired
+    private CandidateDetailsRepository candidateDetailsRepository;
+    @Autowired
+    private CandidateBasicDetailsRepository candidateRepository;
 
-		mockMvc.perform(post("/api/v1/batch-candidates").contentType(MediaType.APPLICATION_JSON)
-				.content(objectMapper.writeValueAsString(batchCandidate)).header("Authorization", authorization))
-				.andExpect(status().isOk()).andExpect(jsonPath("$.batch.id").value(2L))
-				.andExpect(jsonPath("$.candidate.candidateId").value("C124"));
-	}
+    @BeforeEach
+    public void setup() {
+        //creating a candidate
+        if(testingCandidateBD == null){
+            testingCandidateBD = new CandidateBasicDetails();
+            testingCandidateBD.setCandidateId("SDPTEST01");
+            testingCandidateBD.setFirstName("John");
 
-	@Test
-	public void testGetBatchCandidateById() throws Exception {
-		LOGGER.info("Getting a batch candidate for testing");
+            candidateRepository.save(testingCandidateBD);
+        }
+        //creating details for the candidate
+        if (testingCandidate == null) {
+            testingCandidate = new CandidateDetails();
 
-		Long batchCandidateId = 1L;
+            testingCandidate.setCandidateId(testingCandidateBD.getCandidateId());
+            testingCandidate.setAddressCity("test city");
 
-		mockMvc.perform(get("/api/v1/batch-candidates/{id}", batchCandidateId).header("Authorization", authorization))
-				.andExpect(status().isOk());
-		// .andExpect(jsonPath("$.id").value(batchCandidateId));
-	}
+            testingCandidateBD.setDetails(testingCandidate);
+            testingCandidate.setCandidateBasicDetails(testingCandidateBD);
 
-	@Test
-	public void testGetAllBatchCandidates() throws Exception {
-		LOGGER.info("Getting all candidate for testing");
+            candidateRepository.save(testingCandidateBD);
+        }
 
-		mockMvc.perform(get("/api/v1/batch-candidates").header("Authorization", authorization))
-				.andExpect(status().isOk()).andExpect(jsonPath("$").isArray());
-	}
+        if (testingBatch == null) {
+            testingBatch = new Batch();
+            testingBatch.setBatchType("test type");
+            testingBatch.setStatus("test status");
+            testingBatch.setStartDate(new Date());
 
-	@Test
-	public void testUpdateBatchCandidate() throws Exception {
+            batchRepository.save(testingBatch);
+        }
+    }
 
-		LOGGER.info("Updating a batch candidate for testing");
+    @AfterEach
+    public void cleanup(){
+        batchRepository.delete(testingBatch);
+        candidateRepository.delete(testingCandidateBD);
+    }
 
-		Long batchCandidateId = 1L;
-		Date startDate = new Date();
-		Batch batch = new Batch(1L, startDate, "regular", "upcoming");
-		CandidateDetails candidate = new CandidateDetails("C123", null, 0, null, null, null, null, null, null, null,
-				null, null, null, null, null, false, false, false, null);
-		BatchCandidates batchCandidate = new BatchCandidates(batch, candidate);
+    @Test
+    public void bubalamara(){
+        LOGGER.info("mana mana");
+    }
 
-		mockMvc.perform(put("/api/v1/batch-candidates/{id}", batchCandidateId).contentType(MediaType.APPLICATION_JSON)
-				.content(objectMapper.writeValueAsString(batchCandidate)).header("Authorization", authorization))
-				.andExpect(status().isOk()).andExpect(jsonPath("$.batch.id").value(1L))
-				.andExpect(jsonPath("$.candidate.candidateId").value("C123"));
-	}
+    @Test
+    public void testCreateBatchCandidate() throws Exception {
+        LOGGER.info("Creating a batch candidate for testing");
 
-	@Test
-	public void testDeleteBatchCandidate() throws Exception {
-		LOGGER.info("Deleting a batch candidate for testing");
+        Date startDate = new Date();
+        Batch batch = new Batch(2L, startDate, "regular", "upcoming");
+        CandidateDetails candidate = new CandidateDetails("C124", null, 0, null, null, null, null, null, null, null,
+                null, null, null, null, null, false, false, false, null);
+        BatchCandidates batchCandidate = new BatchCandidates(batch, candidate);
 
-		Long batchId = 2L;
-		String candidateId = "C124";
+        mockMvc.perform(post("/api/v1/batch-candidates").contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(batchCandidate)).header("Authorization", authorization))
+                .andExpect(status().isOk()).andExpect(jsonPath("$.batch.id").value(2L))
+                .andExpect(jsonPath("$.candidate.candidateId").value("C124"));
+    }
 
-		mockMvc.perform(delete("/api/v1/batch-candidates/{batchId}/{candidateId}", batchId, candidateId)
-				.header("Authorization", authorization)).andExpect(status().isOk());
-	}
+    @Test
+    public void testGetBatchCandidateById() throws Exception {
+        LOGGER.info("Getting a batch candidate for testing");
+
+        Long batchCandidateId = 1L;
+
+        mockMvc.perform(get("/api/v1/batch-candidates/{id}", batchCandidateId).header("Authorization", authorization))
+                .andExpect(status().isOk());
+        // .andExpect(jsonPath("$.id").value(batchCandidateId));
+    }
+
+    @Test
+    public void testGetAllBatchCandidates() throws Exception {
+        LOGGER.info("Getting all candidate for testing");
+
+        mockMvc.perform(get("/api/v1/batch-candidates").header("Authorization", authorization))
+                .andExpect(status().isOk()).andExpect(jsonPath("$").isArray());
+    }
+
+    @Test
+    public void testUpdateBatchCandidate() throws Exception {
+
+        LOGGER.info("Updating a batch candidate for testing");
+
+        Long batchCandidateId = 1L;
+        Date startDate = new Date();
+        Batch batch = new Batch(1L, startDate, "regular", "upcoming");
+        CandidateDetails candidate = new CandidateDetails("C123", null, 0, null, null, null, null, null, null, null,
+                null, null, null, null, null, false, false, false, null);
+        BatchCandidates batchCandidate = new BatchCandidates(batch, candidate);
+
+        mockMvc.perform(put("/api/v1/batch-candidates/{id}", batchCandidateId).contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(batchCandidate)).header("Authorization", authorization))
+                .andExpect(status().isOk()).andExpect(jsonPath("$.batch.id").value(1L))
+                .andExpect(jsonPath("$.candidate.candidateId").value("C123"));
+    }
+
+    @Test
+    public void testDeleteBatchCandidate() throws Exception {
+        LOGGER.info("Deleting a batch candidate for testing");
+
+        Long batchId = 2L;
+        String candidateId = "C124";
+
+        mockMvc.perform(delete("/api/v1/batch-candidates/{batchId}/{candidateId}", batchId, candidateId)
+                .header("Authorization", authorization)).andExpect(status().isOk());
+    }
 
 }

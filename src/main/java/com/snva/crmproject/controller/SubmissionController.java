f6044f1662dc0f57f34ed88a221163b1b332ebd5
johnchen007 <153539429+johnchen007@users.noreply.github.com>
2024-01-22 15:53:55 -0500

    Fixed one minor issue in Controller

--- src/main/java/com/snva/crmproject/controller/SubmissionController.java
+++ src/main/java/com/snva/crmproject/controller/SubmissionController.java
@@ -6,19 +6,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.snva.crmproject.entity.SubmissionDetails.Submission;
 import com.snva.crmproject.service.SubmissionService;
 
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 @CrossOrigin(origins = "${fontEnd.origins}")
 @RestController

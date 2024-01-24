6b41e62f5b93638c59d863b1960ad01c08cfd7a2
johnchen007 <153539429+johnchen007@users.noreply.github.com>
2024-01-24 12:04:31 -0500

    Update to fix few errors in EndClient Repo

--- src/main/java/com/snva/crmproject/entity/EndClient.java
+++ src/main/java/com/snva/crmproject/entity/EndClient.java
@@ -21,4 +21,44 @@ public class EndClient {
 
     }
 
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getEndClient() {
+		return endClient;
+	}
+
+	public void setEndClient(String endClient) {
+		this.endClient = endClient;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public EndClient(Long id, String endClient, String city, String state) {
+		super();
+		this.id = id;
+		this.endClient = endClient;
+		this.city = city;
+		this.state = state;
+	}
+
 }
--- src/main/java/com/snva/crmproject/repository/candidate/EndClientRepository.java
+++
@@ -1,8 +0,0 @@
-package com.snva.crmproject.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.snva.crmproject.entity.EndClient;
-
-public interface EndClientRepository extends JpaRepository<EndClient, Long> {
-   
-}
